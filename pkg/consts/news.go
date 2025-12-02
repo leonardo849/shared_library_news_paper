3a6ddd713becec0ee910e203cf7823ec08f7468c
leonardo849 <javascriptleonardolucas@gmail.com>
2025-12-02 10:06:58 -0300

    ...

---
+++ pkg/consts/news.go
@@ -0,0 +1,9 @@
+package consts
+
+const (
+	ExchangeDirectNameNewsEvents = "news.auth"
+	KeyAllEvents = "news.*.*"
+	KeyUserNewsCreated = "news.user.created"
+	KeyUserNewsCreatedError = "news.user.created_error"
+	QueueAuthName = "auth_queue"
+)
